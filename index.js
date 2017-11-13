c0586a929207993f968260847f3be0c279ff9e97
Yahya Azouzi <yahya.azouzi@esprit.tn>
2017-11-13 23:16:18 +0100

    version 1.0.5

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ express=require('express');
  routes=require("./routes/api");
  app=express();
 app.use('/api',routes);
+app.set('port', (process.env.PORT || 5000));
 
-app.listen(process.env.port||5000,function () {
-console.log('now listening for requests');
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
 });
+/*app.listen(process.env.port||5000,function () {
+console.log('now listening for requests');
+});*/
